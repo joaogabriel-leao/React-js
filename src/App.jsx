ce9d495955339b45440d91c9d8823ddd716bab31
Joao Gabriel Leao <joao.gabriel.leao1234@gmail.com>
2025-09-30 17:37:08 -0300

    Atualizações na aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ export default function App() {
     <>
       <Banner />
       <Formulario
-      times={times.map(time => time.nome)}
+        times={times.map((time) => time.nome)}
         aoColaboradorCadastrado={(colaborador) =>
           aoNovoColaboradorAdicionado(colaborador)
         }
@@ -63,6 +63,9 @@ export default function App() {
           nome={time.nome}
           corPrimaria={time.corPrimaria}
           corSecundaria={time.corSecundaria}
+          colaboradores={colaboradores.filter(
+            (colaborador) => colaborador.time === time.nome
+          )}
         />
       ))}
     </>
--- src/componentes/Time/Time.jsx
+++ src/componentes/Time/Time.jsx
@@ -1,12 +1,24 @@
+import Colaborador from "../Colaborador/Colaborador";
 import "./Time.css";
 
 const Time = (props) => {
-    const css = { backgroundColor: props.corSecundaria };
-    const cssTitulo = { borderColor: props.corPrimaria };
+  const css = { backgroundColor: props.corSecundaria };
+  const cssTitulo = { borderColor: props.corPrimaria };
   return (
-    <section className="time" style={css}>
-      <h3 style={cssTitulo}>{props.nome}</h3> 
-    </section>
+    (props.colaboradores.length > 0) ?
+      <section className="time" style={css}>
+        <h3 style={cssTitulo}>{props.nome}</h3>
+        <div className="colaboradores">
+          {props.colaboradores.map((colaborador) => (
+            <Colaborador
+              nome={colaborador.nome}
+              imagem={colaborador.imagem}
+              cargo={colaborador.cargo}
+            />
+          ))}
+        </div>
+      </section>
+    : ''
   );
 };
 
